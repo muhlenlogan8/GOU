e966fd850791e768cc77504c9a56a5cc68c5f92c
muhlenlogan8 <muhlenlogan8@gmail.com>
2024-11-10 00:45:21 -0500

    Added google cloud function stuff

--- backend/functions.py
+++ backend/functions.py
@@ -16,3 +16,14 @@ def dist_between_two_points(lat1, lon1, lat2, lon2):
 
 # Test with the given coordinates
 dist_between_two_points(39.134918, -84.519845, 39.135287, -84.511129)
+
+from google.cloud import storage
+
+def getImage():
+    client = storage.Client.from_service_account_json("GoogleCS.json")
+    bucket = client.get_bucket("hackathongeoguesser")
+    
+    imagePath = "images/IMG_20210710_131234.jpg"
+    blob = bucket.blob(imagePath)
+    # Downloads image to file image.jpg then can use image.jpg for stuff
+    blob.download_to_filename("image.jpg")
