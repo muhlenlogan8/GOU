371b7e6ad7ab9f177ebeaaa843af26d3f02c85ec
muhlenlogan8 <muhlenlogan8@gmail.com>
2024-11-27 01:07:45 -0500

    Changing map due to stadia map free tier running out

--- GOU/src/components/Map.jsx
+++ GOU/src/components/Map.jsx
@@ -245,9 +245,8 @@ const Map = forwardRef(({ onCoordinatesSubmit, imagesData, round }, ref) => {
 			>
 				{/* Leaflet map component */}
 				<TileLayer
-					url="https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}"
-					attribution='&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-					ext="jpg"
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				/>
 				<LocationMarker />
 				{/* Add Marker and line if user submits coordinates for the round */}
